06b20aad31663875e25cad02e18ed095899a35f8
Bowen Zhang <2042866313@qq.com>
2026-01-29 09:42:26 +0800

    design ast

---
+++ src/include/ast.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <memory>
+#include <string>
+// 所有 AST 的基类
+class BaseAST {
+ public:
+  virtual ~BaseAST() noexcept = default;
+};
+
+// CompUnit 是 BaseAST
+class CompUnitAST : public BaseAST {
+ public:
+  // 用智能指针管理对象
+  std::unique_ptr<BaseAST> func_def;
+};
+
+// FuncDef 也是 BaseAST
+class FuncDefAST : public BaseAST {
+ public:
+  std::unique_ptr<BaseAST> func_type;
+  std::string ident;
+  std::unique_ptr<BaseAST> block;
+};
+
+// ...
